9b477b5020887705803b4b71ccd9a5ae40a08e1e
Giorgio Borelli <giorgio.borelli@abstract.it>
2018-01-06 11:24:39 +0100

    add babel-polyfill

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
